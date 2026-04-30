3f587ae37876f0944b419c22eeeadec0e014e9aa
Max <156715316+MaxPoshyvak@users.noreply.github.com>
2026-04-30 16:03:06 +0300

    Update email transporter configuration for Gmail

--- src/utils/email.service.ts
+++ src/utils/email.service.ts
@@ -2,7 +2,8 @@ import nodemailer from 'nodemailer';
 import pool from '../config/dataBase/postgreSQL';
 
 const transporter = nodemailer.createTransport({
-    service: 'smtp.gmail.com',
+    host: 'smtp.gmail.com', 
+    port: 465,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
