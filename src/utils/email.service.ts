cde7e287d1d18185d4552f5837f7130a8a2ebc96
Max <156715316+MaxPoshyvak@users.noreply.github.com>
2026-04-30 19:26:25 +0300

    Enable secure transport for email service

--- src/utils/email.service.ts
+++ src/utils/email.service.ts
@@ -4,6 +4,7 @@ import pool from '../config/dataBase/postgreSQL';
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com', 
     port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
